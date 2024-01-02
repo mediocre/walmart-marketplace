dec6b36bbd4f9d17ac867204d9833ad9e670c501
Shawn Miller <shawn@mercatalyst.com>
2024-01-02 00:59:47 -0600

    Update index.js

--- test/index.js
+++ test/index.js
@@ -1,13 +1,24 @@
 const assert = require('assert');
+const crypto = require('crypto');
 const test = require('node:test');
 
 const cache = require('memory-cache');
 
 const WalmartMarketplace = require('../index');
 
-test('WalmartMarketplace.authentication.getAccessToken', async (t) => {
+test('WalmartMarketplace.authentication.getAccessToken(options)', async (t) => {
     t.beforeEach(() => {
         cache.clear();
+    });
+
+    await test('should support options', async () => {
+        const walmartMarketplace = new WalmartMarketplace({
+            clientId: process.env.CLIENT_ID,
+            clientSecret: process.env.CLIENT_SECRET
+        });
+
+        const accessToken = await walmartMarketplace.authentication.getAccessToken({ 'WM_QOS.CORRELATION_ID': crypto.randomUUID() });
+        assert(accessToken);
     });
 
     await test('should cache the access token', async () => {
@@ -25,6 +36,40 @@ test('WalmartMarketplace.authentication.getAccessToken', async (t) => {
         assert.strictEqual(cache.size(), 1);
 
         assert.deepStrictEqual(accessToken1, accessToken2);
+    });
+
+    await test('should return an error for invalid url', async () => {
+        const walmartMarketplace = new WalmartMarketplace({
+            url: 'invalid'
+        });
+
+        try {
+            await walmartMarketplace.authentication.getAccessToken();
+            assert.fail('Expected an error to be thrown');
+        } catch (err) {
+            assert(err);
+            assert.strictEqual(err.message, 'Failed to parse URL from invalid/v3/token');
+        }
+    });
+});
+
+test('WalmartMarketplace.authentication.getAccessToken(options, callback)', async (t) => {
+    t.beforeEach(() => {
+        cache.clear();
+    });
+
+    await test('should support options', function(t, done) {
+        const walmartMarketplace = new WalmartMarketplace({
+            clientId: process.env.CLIENT_ID,
+            clientSecret: process.env.CLIENT_SECRET
+        });
+
+        walmartMarketplace.authentication.getAccessToken({ 'WM_QOS.CORRELATION_ID': crypto.randomUUID() }, function(err, accessToken) {
+            assert.ifError(err);
+            assert(accessToken);
+            
+            done();
+        });
     });
 
     await test('should cache the access token', function(t, done) {
@@ -47,20 +92,6 @@ test('WalmartMarketplace.authentication.getAccessToken', async (t) => {
                 done();
             });
         });
-    });
-
-    await test('should return an error for invalid url', async () => {
-        const walmartMarketplace = new WalmartMarketplace({
-            url: 'invalid'
-        });
-
-        try {
-            await walmartMarketplace.authentication.getAccessToken();
-            assert.fail('Expected an error to be thrown');
-        } catch (err) {
-            assert(err);
-            assert.strictEqual(err.message, 'Failed to parse URL from invalid/v3/token');
-        }
     });
 
     await test('should return an error for invalid url', function(t, done) {
